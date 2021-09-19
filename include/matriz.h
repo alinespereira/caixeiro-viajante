2c605ab8c41772618492ba9678c8eb9a45dd7e79
RGonzalesR <renangonzales@gmail.com>
2021-09-19 18:59:36 -0300

    Update matriz.h

--- include/matriz.h
+++ include/matriz.h
@@ -1,9 +1,15 @@
 #ifndef _MATRIZ_H
 #define _MATRIZ_H
 
+#include <stdbool.h>
+
 typedef struct _matriz MATRIZ;
 
 MATRIZ *matriz_criar(int tamanho);
 int matriz_buscar_entrada(MATRIZ *m, int i, int j);
+bool matriz_preencher(MATRIZ *m);
+bool matriz_vazia (MATRIZ *m);
+void matriz_limpar (MATRIZ **m);
+void matriz_imprimir (MATRIZ *m);
 
 #endif
