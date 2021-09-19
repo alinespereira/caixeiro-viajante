473cf2a10fb17fa747db128f1b836fca3b137c62
RGonzalesR <renangonzales@gmail.com>
2021-09-19 18:59:22 -0300

    Update main.c
    O problema, aparentemente, está em como receber o tamanho da matriz.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 #include "adjacence-list.h"
+#include "matriz.h"
+#include <stdio.h>
 
 int main()
 {
+    MATRIZ *m;
+
+    printf("teste");
+    
+    m = matriz_criar(3);
+    matriz_imprimir(m);
+
+    matriz_preencher(m);
+
+    matriz_imprimir(m);
+
+    matriz_limpar(&m);
+
     return 0;
 }
