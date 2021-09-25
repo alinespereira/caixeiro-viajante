300c817c3ac173a3e0347c92409889b832b625db
Aline <line.asramos@gmail.com>
2021-09-25 17:24:57 -0300

    Caixeiro TAD basic structure

---
+++ include/caixeiro.h
@@ -0,0 +1,13 @@
+#ifndef _CAIXEIRO_H
+#define _CAIXEIRO_H
+
+#include "matriz.h"
+#include "vetor.h"
+
+typedef struct _caminho CAMINHO;
+
+MATRIZ *caixeiro_ler_dados();
+CAMINHO *caixeiro_calcular_menor_caminho();
+int caixeiro_calcular_distancia(MATRIZ *custos, CAMINHO *caminho);
+
+#endif
