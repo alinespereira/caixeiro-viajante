cf236f9fd96d2bea234fa9b0ef4b9348978fbebf
Aline <line.asramos@gmail.com>
2021-09-26 20:06:19 -0300

    Create functions in vetor and adjust naming

--- src/vetor.c
+++ src/vetor.c
@@ -23,27 +23,90 @@ VETOR *vetor_criar(int tamanho)
     return NULL;
 }
 
-int vetor_get_tamanho(VETOR *vetor)
+VETOR *vetor_copiar(VETOR *v)
 {
-    return vetor->tamanho;
+    VETOR *tmp = vetor_criar(v->tamanho);
+
+    for (int i = 0; i < v->tamanho; i++)
+        tmp->dados[i] = v->dados[i];
+
+    return tmp;
 }
 
-bool vetor_set_elemento(VETOR *vetor, int indice, int valor)
+int vetor_get_tamanho(VETOR *v)
 {
-    if (indice < vetor->tamanho)
+    if (v != NULL)
+        return v->tamanho;
+
+    return INT_MIN;
+}
+
+bool vetor_set_elemento(VETOR *v, int indice, int valor)
+{
+    if (indice < v->tamanho)
     {
-        vetor->dados[indice] = valor;
+        v->dados[indice] = valor;
         return true;
     }
 
     return false;
 }
 
-int vetor_get_elemento(VETOR *vetor, int indice)
+int vetor_get_elemento(VETOR *v, int indice)
 {
-    return vetor->dados[indice];
+    return v->dados[indice];
 }
 
-void vetor_apagar(VETOR **vetor)
+bool vetor_push_elemento(VETOR *v, int valor)
+{
+    v->dados = (int *)realloc(v->dados, (v->tamanho + 1) * sizeof(int));
+    if (v->dados == NULL)
+        return false;
+
+    v->dados[v->tamanho] = valor;
+    v->tamanho++;
+    return true;
+}
+
+int vetor_pop_elemento(VETOR *v)
+{
+    int valor = vetor_get_elemento(v, v->tamanho - 1);
+    v->tamanho--;
+    v->dados = (int *)realloc(v->dados, v->tamanho * sizeof(int));
+
+    if (v->dados == NULL)
+        return INT_MAX;
+
+    return valor;
+}
+
+VETOR *vetor_remover_elemento(VETOR *v, int indice)
+{
+    VETOR *tmp = vetor_copiar(v);
+
+    if (indice >= tmp->tamanho)
+        return NULL;
+
+    for (int i = indice; i < tmp->tamanho - 1; i++)
+    {
+        int proximo = tmp->dados[i + 1];
+        tmp->dados[indice] = proximo;
+    }
+
+    tmp->dados = (int *)realloc(tmp->dados, (tmp->tamanho - 1) * sizeof(int));
+    tmp->tamanho--;
+
+    return tmp;
+}
+
+void vetor_imprimir(VETOR *v)
+{
+    printf("\t\tvetor: [%d", vetor_get_elemento(v, 0));
+    for (int i = 1; i < v->tamanho; i++)
+        printf(", %d", vetor_get_elemento(v, i));
+    printf("]\n");
+}
+
+void vetor_apagar(VETOR **v)
 {
 }
