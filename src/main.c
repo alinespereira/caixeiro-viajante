8716c03ed49254dc89ce5c0deab00c7875e5034c
Aline <line.asramos@gmail.com>
2021-09-26 20:07:52 -0300

    Test PCV solver

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+#include "vetor.h"
 #include "matriz.h"
 #include "caixeiro.h"
 
 int main()
 {
     MATRIZ *distancias = caixeiro_ler_dados();
+    int n_caminhos = 0;
+    CAMINHO **caminho = NULL;
+    int n_cidades = matriz_get_tamanho(distancias);
+    VETOR *disponiveis = vetor_criar(n_cidades - 1);
+
+    for (int i = 1; i < n_cidades; i++)
+        vetor_set_elemento(disponiveis, i - 1, i);
 
     matriz_imprimir(distancias);
+
+    caixeiro_calcular_caminhos(distancias, disponiveis, caminho, &n_caminhos);
 
     matriz_limpar(distancias);
 
